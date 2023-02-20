f676bf0abf2c4b2618b2b6048dcd2d87ca7ec12e
Falcion <57592842+Falcion@users.noreply.github.com>
2023-02-20 23:37:00 +0300

    Reduce of math problems in assignment

--- .programming/IP01/OP1/Figure.cpp
+++ .programming/IP01/OP1/Figure.cpp
@@ -14,74 +14,54 @@ Figure::Figure(float x1, float x2, float x3, float x4,
 	this->y3 = y3;
 	this->y4 = y4;
 
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
+	this->side1 = calculate_side(x1, x2, y1, y2);
+	this->side2 = calculate_side(x2, x3, y2, y3);
+	this->side3 = calculate_side(x3, x4, y3, y4);
+	this->side4 = calculate_side(x1, x4, y1, y4);
+
+	float diag1 = calculate_side(x1, x3, y1, y3);
+	float diag2 = calculate_side(x2, x4, y2, y4);
 
 	this->P = side1 + side2 + side3 + side4;
 
-	float p = 0.5 * (side1 + side2 + side3 + side4);
-	
-	float s = sqrt(p * (p - side1) * (p - side2) * (p - side3) * (p - side4));
+	float coords_sumpo = (x1 + x3) * (x2 + x4) + (y1 * y3) * (y2 * y4);
+	float coords_define = sqrt(pow(x1 + x3, 2) + pow(y1 + y3, 2)) * sqrt(pow(x2 + x4, 2) + pow(y2 + y4, 2));
 
-	this->S = s;
+	float cos = coords_sumpo / coords_define;
+
+	float sin = sqrt(abs(1 - pow(cos, 2)));
+
+	this->S = 0.5 * diag1 * diag2 * sin;
 }
 
 float Figure::calculate_side(float x1, float x2, float y1, float y2) {
 	float x12 = abs(x2 - x1);
 	float y12 = abs(y2 - y1);
 
-	float xy = sqrt(pow(x12, 2) + pow(y12, 2));
-
-	return xy;
+	return sqrt(pow(x12, 2) + pow(y12, 2));
 }
 
 bool Figure::is_prug() {
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-
-	float sin = S / (side1 * side2);
+	float sin = S / (this->side1 * this->side2);
 
 	double degrees = asin(sin);
 
-	if (degrees > 89.7 && degrees < 90.3)
-		return true;
-	else
-		return false;
+	return (degrees > 89.7 && degrees < 90.3);
 }
 
 void Figure::show() {
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
-
 	cout << "Sides: " << side1 << ", " << side2 << ", " << side3 << ", " << side4 << ";" << endl;
 	cout << "Square: " << this->S << ", " << "perimeter: " << this->P << endl;
 }
 
 bool Figure::is_square() {
 	bool is_pr = is_prug();
-
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
 	
-	if (side1 == side2 == side3 == side4 && is_pr)
-		return true;
-	else
-		return false;
+	return (side1 == side2 == side3 == side4 && is_pr);
 }
 
 bool Figure::is_romb() {
 	bool is_pr = is_prug();
-
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
 
 	if (side1 == side2 == side3 == side4 && is_pr == false)
 		return true;
@@ -90,28 +70,11 @@ bool Figure::is_romb() {
 }
 
 bool Figure::is_in_circle() {
-	float diag1 = calculate_side(x1, x3, y1, y3);
-	float diag2 = calculate_side(x2, x4, y2, y4);
-
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
-
-	if (diag1 * diag2 == (side1 * side3 + side2 * side4))
-		return true;
-	else
-		return false;
+	float P1 = this->P / 2;
+	float S1 = sqrt((P1 - side1) * (P1 - side2) * (P1 - side3) * (P1 - side4));
+	return (S1 == S);
 }
 
 bool Figure::is_out_circle() {
-	float side1 = calculate_side(x1, x2, y1, y2);
-	float side2 = calculate_side(x2, x3, y2, y3);
-	float side3 = calculate_side(x3, x4, y3, y4);
-	float side4 = calculate_side(x1, x4, y1, y4);
-
-	if (side1 + side3 == side2 + side4)
-		return true;
-	else
-		return false;
+	return (side1 + side3 == side2 + side4);
 }
