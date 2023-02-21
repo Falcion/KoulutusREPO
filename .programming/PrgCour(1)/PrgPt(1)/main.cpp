b0ee2b723191ecc0dc02eaeb34c3c7e7eefa320d
Falcion <falcionml@gmail.com>
2023-02-21 14:20:20 +0300

    Refactor of TASK-1

---
+++ .programming/PrgCour(1)/PrgPt(1)/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "triangle.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    triangle triangles[3];
+
+    double a;
+    double b;
+    double c;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Enter three sides length for triangle #" << i + 1 << " separated by space: ";
+
+        cin >> a >> b >> c;
+
+        triangles[i].set(a, b, c);
+
+        if (!triangles[i].exst_tr()) {
+            triangles[i].show();
+
+            cout << "This triangle doesn't exist, please, try again!" << endl;
+
+            i--;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        triangles[i].show();
+
+        cout << "[TRIANGLE] Perimeter (#" << i + 1 << "): " << triangles[i].perimeter() << ";" << endl;
+        cout << "[TRIANGLE] Square (#" << i + 1 << "): " << triangles[i].square() << ";" << endl;
+    }
+}
---
+++ .programming/PrgCour(1)/PrgPt(1)/triangle.h
@@ -0,0 +1,12 @@
+class triangle {
+private:
+	float a;
+	float b;
+	float c;
+public:
+	void show();
+	float perimeter();
+	float square();
+	bool exst_tr();
+	void set(float a, float b, float c);
+};
